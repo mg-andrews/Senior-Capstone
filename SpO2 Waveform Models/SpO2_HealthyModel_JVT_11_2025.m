1314d8746dfd7ba28696e490e63a14ee7830112a
JoshuaVanTuyl <joshuafvantuyl@gmail.com>
2025-11-19 16:50:09 -0800

    updates
    updates

--- SpO2 Waveform Models/SpO2_HealthyModel_JVT_11_2025.m
+++ SpO2 Waveform Models/SpO2_HealthyModel_JVT_11_2025.m
@@ -2,4 +2,12 @@ clear
 clc
 
 %Load up the file of pulse oximetry data
-%
+%bloodgas.csv
+
+
+%Select the column that has SpO2 values
+%Save to new array
+%Connect new array to skin tone
+
+%Let n = some random number between 1 and len(array)
+%Whatever that value is, display it as output
