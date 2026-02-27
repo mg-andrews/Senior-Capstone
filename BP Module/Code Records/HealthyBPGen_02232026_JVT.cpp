61154a5169f320d772be3d1491e69edc9fe4a401
JoshuaVanTuyl <joshuafvantuyl@gmail.com>
2026-02-27 13:22:35 -0800

    BP Model C++ Transfer
    Creating updated BP data generation model in C++

---
+++ BP Module/Code Records/HealthyBPGen_02232026_JVT.cpp
@@ -0,0 +1,24 @@
+//LIBRARIES
+#include <iostream>
+#include <cmath>
+
+// GLOBAL VARIABLES
+
+
+
+
+
+// FUNCTIONS
+
+// Need to define a function where it takes in a time t and a bpm and returns systolic pressure at time t
+
+// Need to define a function where it takes in a time t and a bpm and returns a diastolic pressure at time t
+
+// Need to define a gaussian scaling function
+
+// Need to define an exponential decay function 
+
+int main() {
+    return 0;
+}
+
---
+++ BP Module/Code Records/test.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    double x = std::sqrt(25.0);
+    std::cout << x << std::endl;
+
+    return 0;
+}
