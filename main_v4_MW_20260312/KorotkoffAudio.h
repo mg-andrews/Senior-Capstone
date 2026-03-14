53321f48563e6066c6c84b71b4b9773aa26c1724
milson-lab <nosliwbrix@gmail.com>
2026-03-13 17:58:43 -0700

    Added korotkoff sound audio integration with main
    Also created separate header files for pin definitions, audio playback functions, and the motor setup

---
+++ main_v4_MW_20260312/KorotkoffAudio.h
@@ -0,0 +1,10 @@
+#ifndef KorotkoffAudio_H
+#define KorotkoffAudio_H
+
+#include <Arduino.h>
+
+void setup_i2s();
+void audio_process();
+void start_beat();
+
+#endif
---
+++ main_v4_MW_20260312/pindef.h
@@ -0,0 +1,25 @@
+#ifndef PINS_H
+#define PINS_H
+
+// Pump sim module
+#define POT_PIN 10
+#define V_PLUS 39
+#define BUTTON_PIN 38
+#define VIO_PIN 15
+
+// Motor driver
+#define EN_PIN   16
+#define STEP_PIN 17
+#define DIR_PIN  18
+#define UART_TX  5
+#define UART_RX  6
+
+// LED
+#define HRLED 36
+
+// I2S comms pins for audio
+#define I2S_BCLK 3
+#define I2S_LRC  8
+#define I2S_DIN  46
+
+#endif
